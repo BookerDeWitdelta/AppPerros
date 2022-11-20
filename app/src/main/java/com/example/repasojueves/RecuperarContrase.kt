02615bf1aaf9fce30d2392dae3ca6bdaaa9806ec
Andrea Piza <carmenandreapiza@gmail.com>
2022-11-19 23:54:35 -0500

    Recuperar Contraseña

--- app/src/main/java/com/example/repasojueves/RecuperarContrase.kt
+++ app/src/main/java/com/example/repasojueves/RecuperarContrase.kt
@@ -2,10 +2,31 @@ package com.example.repasojueves
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.repasojueves.databinding.ActivityRecuperarContraseBinding
 
 class RecuperarContrase : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recuperar_contrase)
-    }
-}
+   // private lateinit var binding: ActivityRecuperarContraseBinding
+    //private lateinit var firebaseAuth:FirebaseAuth
+    //override fun onCreate(savedInstanceState: Bundle?) {
+        //binding=ActivityRecuperarContraseBinding.inflate(layoutInflater)
+        //super.onCreate(savedInstanceState)
+        //setContentView(binding.root)
+        //firebaseAuth=Firebase.auth
+        //binding.editTextTextEmailAddress.setOnClickListener{
+            //recuperarcontraseña(binding.editTextTextEmailAddress.text.toString())
+        }
+
+    //}
+    //private fun recuperarcontraseña(email:String){
+       // firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(){
+        //        task->
+           // if (task.isSuccesful){
+             //   Toast.makeText(this,"El correo de recuperacion fue enviado" ,Toast.LENGTH_LONG).show()
+           // }
+            //else{
+              //  Toast.makeText(this, "No fue posible enviar el correo" ,Toast.LENGTH_LONG).show()
+            //}
+       // }
+  //  }
+//}
